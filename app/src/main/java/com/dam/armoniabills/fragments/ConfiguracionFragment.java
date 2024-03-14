97b15738458eab5680a9e56211341e735f919304
nachomf18 <118490509+nachomf18@users.noreply.github.com>
2024-03-14 16:39:13 +0100

    Añadir usuarios una vez creado el grupo.

--- app/src/main/java/com/dam/armoniabills/fragments/ConfiguracionFragment.java
+++ app/src/main/java/com/dam/armoniabills/fragments/ConfiguracionFragment.java
@@ -11,6 +11,7 @@ import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -22,6 +23,7 @@ import com.dam.armoniabills.R;
 import com.dam.armoniabills.TopBarActivity;
 import com.dam.armoniabills.model.Grupo;
 import com.dam.armoniabills.model.Usuario;
+import com.dam.armoniabills.model.UsuarioGrupo;
 import com.dam.armoniabills.recyclerutils.AdapterUsuariosGrupo;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -40,8 +42,12 @@ public class ConfiguracionFragment extends Fragment implements View.OnClickListe
     private static final String ARG_GRUPO = "grupo";
     private Grupo grupo;
 
-    EditText etTitulo, etDescripcion;
-    Button btnEditar, btnEliminar;
+    EditText etTitulo, etDescripcion, etPersona;
+    Button btnEditar, btnEliminar, btnAniadir;
+
+    ArrayList<Usuario> listaUsuarios;
+    ArrayList<UsuarioGrupo> listaUsuarioGrupo;
+    Usuario usuarioAniadir;
 
     public ConfiguracionFragment() {
         // Required empty public constructor
@@ -70,14 +76,20 @@ public class ConfiguracionFragment extends Fragment implements View.OnClickListe
 
         etTitulo = v.findViewById(R.id.etTituloConfig);
         etDescripcion = v.findViewById(R.id.etDescripcionConfig);
+        etPersona = v.findViewById(R.id.etAniadirPersonaConfig);
         btnEditar = v.findViewById(R.id.btnUpdateConfig);
         btnEliminar = v.findViewById(R.id.btnEliminar);
+        btnAniadir = v.findViewById(R.id.btnAniadirConfig);
 
         etTitulo.setText(grupo.getTitulo());
         etDescripcion.setText(grupo.getDescripcion());
 
+        listaUsuarios = new ArrayList();
+        listaUsuarioGrupo = new ArrayList<>();
+
         btnEliminar.setOnClickListener(this);
         btnEditar.setOnClickListener(this);
+        btnAniadir.setOnClickListener(this);
 
         return v;
     }
@@ -88,7 +100,98 @@ public class ConfiguracionFragment extends Fragment implements View.OnClickListe
             mostrarDialogEliminar();
         } else if (v.getId() == R.id.btnUpdateConfig) {
             updateGrupo();
+        } else if (v.getId() == R.id.btnAniadirConfig) {
+            readUsuario();
         }
+    }
+
+    private void readUsuario() {
+        listaUsuarioGrupo = grupo.getUsuarios();
+
+        FirebaseDatabase.getInstance().getReference("Usuarios").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot data : snapshot.getChildren()) {
+                    Usuario usuario = data.getValue(Usuario.class);
+                    listaUsuarios.add(usuario);
+                }
+
+                String email = etPersona.getText().toString();
+
+                if (!email.isEmpty() && Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                    boolean usuarioEncontrado = false;
+
+                    for (Usuario usuario : listaUsuarios) {
+                        if (usuario.getEmail().equals(email)) {
+                            usuarioEncontrado = true;
+                            usuarioAniadir = usuario;
+                            break;
+                        }
+                    }
+
+                    if (usuarioEncontrado) {
+                        for (UsuarioGrupo usuarioGrupo : listaUsuarioGrupo) {
+                            if (usuarioGrupo.getId().equals(usuarioAniadir.getId())) {
+                                usuarioEncontrado = true;
+                                break;
+                            } else {
+                                usuarioEncontrado = false;
+                            }
+                        }
+
+                        if (!usuarioEncontrado) {
+                            listaUsuarioGrupo.add(new UsuarioGrupo(0, 0, 0, usuarioAniadir.getId()));
+                            aniadirUsuario();
+                        } else {
+                            Toast.makeText(getContext(), getString(R.string.usuario_grupo), Toast.LENGTH_SHORT).show();
+                        }
+                    } else {
+                        Toast.makeText(getContext(), getString(R.string.user_no_registrado), Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(getContext(), getString(R.string.email_incorrecto), Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(getContext(), error.toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void aniadirUsuario() {
+        ArrayList<String> listaGrupos = new ArrayList<>();
+        listaGrupos = usuarioAniadir.getGrupos();
+
+        if (listaGrupos == null) {
+            listaGrupos = new ArrayList<>();
+            listaGrupos.add(grupo.getId());
+        } else {
+            listaGrupos.add(grupo.getId());
+        }
+
+        FirebaseDatabase.getInstance().getReference("Usuarios").child(usuarioAniadir.getId()).child("grupos").setValue(listaGrupos)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        updateGrupoUsuario();
+                    }
+                });
+    }
+
+    private void updateGrupoUsuario() {
+        Map<String, Object> mapa = new HashMap<>();
+        mapa.put("usuarios", listaUsuarioGrupo);
+
+        FirebaseDatabase.getInstance().getReference("Grupos").child(grupo.getId()).updateChildren(mapa)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        etPersona.setText("");
+                        Toast.makeText(getContext(), "Usuario añadido correctamente", Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 
     private void mostrarDialogEliminar() {
